df07a7ec7e0732c5c72f2c9184a32aec11fcf731
TIAGO COLADONATO <46878457@ort>
2023-03-20 10:31:34 -0300

    ff

---
+++ Pizzas.API/Controllers/OMDB.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pizzas.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OMDBController : ControllerBase
+{
+   
+
+
+
+   
+}
---
+++ Pizzas.API/Models/ImdbEntity.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Pizzas.API.Models
+{
+    public class ImdbIdentity
+    {
+        public class ImdbEntity
+     {
+      public string Title { get; set; }
+      public string Year { get; set; }
+      public string Rated { get; set; }
+      public string Released { get; set; }
+      public string Runtime { get; set; }
+      public string Genre { get; set; }
+      public string Director { get; set; }
+      public string Writer { get; set; }
+      public string Actors { get; set; }
+      public string Plot { get; set; }
+      public string Language { get; set; }
+      public string Country { get; set; }
+      public string Awards { get; set; }
+      public string Poster { get; set; }
+      public string Metascore { get; set; }
+      public string imdbRating { get; set; }
+      public string imdbVotes { get; set; }
+      public string imdbID { get; set; }
+      public string Type { get; set; }
+      public string Response { get; set; }
+  }
+
+
+        
+    }
+}
